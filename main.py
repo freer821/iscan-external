1c6a8546745fe1c2b1c11399229b6f9a2edf6ac5
Zhenyu Geng <zhenyu.geng@deutschebahn.com>
2023-10-21 22:50:53 +0200

    update

--- main.py
+++ main.py
@@ -19,25 +19,18 @@ def images_to_pdf(image_files, pdf_output):
         imgs[0].save(pdf_output, "PDF", save_all=True, append_images=imgs[1:])
 
 
-def generate_pdf(output_type, image_folder):
-    image_files = [file for file in os.listdir(image_folder) if file.endswith(('.jpeg', '.tiff'))]
-    image_full_paths = [os.path.join(image_folder, filename) for filename in image_files]
-
-    if not image_full_paths:
+def generate_pdf(input_files, output_type, output_path):
+    if not input_files:
         print("No image files found in the folder.")
         return
 
-    pdf_folder = image_folder + '/pdf'
-    if not os.path.isdir(pdf_folder):
-        os.makedirs(pdf_folder)
-
     if output_type == 'single':
-        pdf_output = pdf_folder + '/iscan.pdf'
-        images_to_pdf(image_full_paths, pdf_output)
+        pdf_output = output_path + '/iscan.pdf'
+        images_to_pdf(input_files, pdf_output)
     elif output_type == 'multi':
-        for image in image_full_paths:
+        for image in input_files:
             base_name = os.path.splitext(os.path.basename(image))[0]
-            pdf_output = pdf_folder + '/' + base_name + '.pdf'
+            pdf_output = output_path + '/' + base_name + '.pdf'
             image_to_pdf(image, pdf_output)
     else:
         print('unknown output type!')
@@ -47,11 +40,12 @@ def main(args):
     # Access and use the parameters
     action = args.action
     output_type = args.output_type
-    path = args.path
+    input_files = args.input_files
+    output_path = args.output_path
 
     # Perform some action with the parameters
     if action == 'PDF':
-        generate_pdf(output_type, path)
+        generate_pdf(input_files, output_type, output_path)
     else:
         print('unknown action! program aborts!')
 
@@ -61,9 +55,10 @@ if __name__ == "__main__":
 
     # Define your parameters here
     parser.add_argument("--action", type=str, required=True, default='PDF', help="choose action: PDF")
+    parser.add_argument("--input_files", type=str, required=True, nargs='+', help="choose action: PDF")
     parser.add_argument("--output_type", type=str, required=True,
                         default='single', help="type of action: single of PDF or multi pdfs ")
-    parser.add_argument("--path", type=str, required=True, help="the location of images")
+    parser.add_argument("--output_path", type=str, required=True, help="the location of images")
 
     args = parser.parse_args()
     main(args)
